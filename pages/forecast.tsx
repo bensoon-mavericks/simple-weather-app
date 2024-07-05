388e301349550959bab7f3a9c006460cae4fc360
Ben Soon <benjamin@mavericks-consulting.com>
2024-07-05 11:53:40 +0800

    add-preload-and-refresh-intervals

--- pages/forecast.tsx
+++ pages/forecast.tsx
@@ -3,8 +3,8 @@ import Card from "@/components/card";
 import { CardsSkeleton,CardSkeleton } from "@/components/skeletons";
 import { useRouter } from "next/router";
 import { useEffect, useState, Suspense, Component } from "react";
-import useSWR from "swr";
-
+import useSWR, { preload } from "swr";
+import { getDayOfWeek } from "@/utils/datetime";
 export class ForecastData {
     public constructor(public date: string, public prediction: string) {}
 
@@ -19,16 +19,23 @@ const fetcher = (url) => fetch(url).then(async res => {
     return res.json()
 })
 
+preload('/api/forecast', fetcher)
+
 const onErrorRetry = (error, key, config, revalidate, { retryCount }) => {
     // Retry after 2 seconds.
     setTimeout(() => revalidate({ retryCount }), 2000)
 };
 
-// TODO: change date string to weekday
+
 export default function Forecast() {
     const router = useRouter()
-    const { data: weatherData, error, isLoading } = useSWR('/api/forecast', fetcher, { onErrorRetry })
-    console.log(weatherData)
+    const { data: weatherData, error, isLoading } = useSWR('/api/forecast', fetcher, { 
+        onErrorRetry,
+        dedupingInterval: 600000,
+        refreshInterval: 600000,
+        revalidateOnFocus: false,
+        revalidateIfStale: false, // Do not revalidate if the data is stale
+    })
     return (
         <div>
             <main>
@@ -37,11 +44,11 @@ export default function Forecast() {
                         <Button title="back" onClick={() => router.push("/")}></Button>
                     </div>
                     <div className="min-h-screen flex flex-col items-center justify-center">
-                        <h1 className="text-6xl text-center font-bold pb-24 text-gray-700">Predictions for the next 4 days</h1>
+                        <h1 className="text-6xl text-center font-bold pb-24 text-gray-700">Next 4 days</h1>
                         <div className="w-[60%] flex flex-row gap-4">
                             {(error || isLoading) && <CardsSkeleton />}
                             {weatherData &&
-                                weatherData.items.map(data => <Card data={new ForecastData(data.date, data.prediction)} additionalClassNames="flex-1" />)
+                                weatherData.items.map(data => <Card key={data.date} data={new ForecastData(getDayOfWeek(data.date), data.prediction)} additionalClassNames="flex-1" />)
                             }
                         </div>
                     </div>
